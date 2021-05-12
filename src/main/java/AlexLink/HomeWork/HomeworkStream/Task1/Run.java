94220d3544f7fa04ac1245a1651cb89e3a41e1dc
qpuhuk <qpuhuk@mail.ru>
2021-05-12 22:26:45 +0300

    HWStream

--- src/main/java/AlexLink/HomeWork/HomeworkStream/Task1/Run.java
+++ src/main/java/AlexLink/HomeWork/HomeworkStream/Task1/Run.java
@@ -3,7 +3,8 @@ package AlexLink.HomeWork.HomeworkStream.Task1;
 @FunctionalInterface
 public interface Run {
     void run(Animal pet);
-    default void info() {
-        System.out.println("Object running");
+
+    default void info(Animal pet) {
+        System.out.println(pet.getClass().getSimpleName() + " running");
     }
 }
--- src/main/java/AlexLink/HomeWork/HomeworkStream/Task1/Task1.java
+++ src/main/java/AlexLink/HomeWork/HomeworkStream/Task1/Task1.java
@@ -3,14 +3,19 @@ package AlexLink.HomeWork.HomeworkStream.Task1;
 //Создать свой функциональный интерфейс для вывода на консоль данных объекта.
 //С помощью лямбда-выражения вывести данные для объектов типа String и Integer.
 
+import java.sql.SQLOutput;
+
 public class Task1 {
     public static void main(String[] args) {
 
         Animal cat = new Cat("Murzik", 10);
         Animal dog = new Dog("Jessi", 20);
-        Run run = a -> System.out.println("Speed of the " + a.getName() + " = " + a.getSpeed());
+        Run runCat = a -> System.out.println("His speed = " + a.getSpeed());
+        Run runDog = a -> System.out.println("Her name = " + a.getName());
 
-        run.run(cat);
-        run.run(dog);
+        runCat.info(cat);
+        runCat.run(cat);
+        runDog.info(dog);
+        runDog.run(dog);
     }
 }
