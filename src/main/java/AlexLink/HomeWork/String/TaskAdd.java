9a00e573e69e130b47a335341eb449a0122aabbc
qpuhuk <qpuhuk@mail.ru>
2021-06-01 00:30:30 +0300

    Correct1

--- src/main/java/AlexLink/HomeWork/String/TaskAdd.java
+++ src/main/java/AlexLink/HomeWork/String/TaskAdd.java
@@ -6,15 +6,13 @@ package AlexLink.HomeWork.String;
 //Подсчитать общее их количество.
 
 
-import java.util.Arrays;
-
 public class TaskAdd {
     public static void main(String[] args) {
-        final String SENTENCE = "Ехал я по городу и услышал топот! Обернулся, а там стая лошадей. " +
+        String sentence = "Ехал я по городу и услышал топот! Обернулся, а там стая лошадей. " +
                 "Стоят и смотрят на меня завораживающе. Я поехал дальше. Когда доехал до водопада, увидел, что там потоп?";
         String pattern = "[,.!?:;\\-]";
-        int before = SENTENCE.length();
-        int after = SENTENCE.replaceAll(pattern, "").length();
+        int before = sentence.length();
+        int after = sentence.replaceAll(pattern, "").length();
         int count = before - after;
         System.out.println("Количество знаков препинания в тексте = " + count);
 
@@ -23,11 +21,11 @@ public class TaskAdd {
 
         StringBuilder stringBuilder = new StringBuilder();
         String pattern2 = "\\s|[,.!?]\\s|[,.!?]";
-        String[] divSentence = SENTENCE.split(pattern2);
+        String[] divSentence = sentence.split(pattern2);
         for (String temp : divSentence) {
             String word = temp.trim();
             stringBuilder.append(word.charAt(word.length() - 1));
         }
-        System.out.println("Текст, составленный из последних букв всех слов ----" + stringBuilder.toString());
+        System.out.println("Текст, составленный из последних букв всех слов ----" + stringBuilder);
     }
 }
