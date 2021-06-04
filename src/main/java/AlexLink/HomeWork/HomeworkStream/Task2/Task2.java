b9a4377bdd90bf952390cc426560f709779fca01
qpuhuk <qpuhuk@mail.ru>
2021-06-04 22:56:21 +0300

    Correct2

--- src/main/java/AlexLink/HomeWork/HomeworkStream/Task2/Task2.java
+++ src/main/java/AlexLink/HomeWork/HomeworkStream/Task2/Task2.java
@@ -4,6 +4,7 @@ package AlexLink.HomeWork.HomeworkStream.Task2;
 //лексикографическом порядке по возрастанию и убыванию
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Task2 {
     static final String TEXT = "Задать массив строк. Используя средства Stream API отсортировать строки в\n" +
@@ -12,11 +13,11 @@ public class Task2 {
     public static void main(String[] args) {
         String[] list = TEXT.split("[.,]?\\s+");
         Arrays.stream(list)
-                .sorted(String::compareToIgnoreCase)
+                .sorted(Comparator.naturalOrder())
                 .forEach(System.out::println);
         System.out.println("******************************************");
         Arrays.stream(list)
-                .sorted((a, b) -> b.compareToIgnoreCase(a))
+                .sorted(Comparator.reverseOrder())
                 .forEach(System.out::println);
     }
 }
