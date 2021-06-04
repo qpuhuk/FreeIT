c44e4709735f1a777507144d77b9c78921198c5a
qpuhuk <qpuhuk@mail.ru>
2021-06-04 22:36:42 +0300

    Correct2

--- src/main/java/AlexLink/HomeWork/Exception/StartException.java
+++ src/main/java/AlexLink/HomeWork/Exception/StartException.java
@@ -1,6 +1,6 @@
 package AlexLink.HomeWork.Exception;
 
-public class StartException extends Exception {
+public class StartException extends RuntimeException {
 
     public StartException(String message) {
         super(message);
